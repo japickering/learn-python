572ac3718329a8464f3a1246e380b1b0f8cb1902
japickering <jpuidev@outlook.com>
2026-09-02 15:41:51 +0100

    Build a User Configuration Manager

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Build a User Configuration Manager
+from libs.configure import add_setting, view_settings, delete_setting
+
+test_settings = {
+    'theme': 'light',
+    'volume': 'high'
+}
+
+print(add_setting(test_settings, ('notifications', 'enabled')))
+print(delete_setting(test_settings, ('notifications')))
+print(view_settings(test_settings))
